4de08f6c11509d625dcfe53cb64d7157a9a5aa57
geddie212 <geddie212>
2022-06-13 15:14:51 +0100

    os environ update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 import keyboard
 import time
+import os
 
 app = Flask(__name__)
 
-time_left = 10
+time_left = os.environ.get('TIMER')
 timer_started = False
 timer_clicked = False
 
@@ -23,7 +24,7 @@ def show_result():
     global time_left
     global timer_started
     global timer_clicked
-    time_left = 10
+    time_left = os.environ.get('TIMER')
     timer_started = False
     text = request.form['text-box']
     return render_template('result.html', text=text, timer_clicked=timer_clicked)
@@ -42,7 +43,7 @@ def start():
     global timer_clicked
     timer_clicked = True
     timer_started = True
-    time_left = 10
+    time_left = os.environ.get('TIMER')
     countdown()
     return render_template('start.html')
 
@@ -54,13 +55,13 @@ def home():
     global timer_clicked
     timer_clicked = False
     timer_started = False
-    time_left = 10
+    time_left = os.environ.get('TIMER')
     return render_template('index.html', time_left=time_left)
 
 
 def on_press(key):
     global time_left
-    time_left = 10
+    time_left = os.environ.get('TIMER')
 
 
 if __name__ == '__main__':
